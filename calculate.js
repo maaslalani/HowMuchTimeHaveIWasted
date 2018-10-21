d40ef7af39337b1c14769b7a66dff63f830bec1d
Darshil Patel <darshil.patel@shopify.com>
2018-10-21 03:11:14 -0400

    add meta tags and twitter share

--- calculate.js
+++ calculate.js
@@ -26,3 +26,13 @@ OLD_WASTED_YEARS = OLD_WASTED_HOURS / 24 / 365
 document.querySelector('#life-wasted').innerText = Math.round(WASTED)
 document.querySelector('#old-life-wasted-hours').innerText = Math.round(OLD_WASTED_HOURS)
 document.querySelector('#old-life-wasted-years').innerText = Math.round(OLD_WASTED_YEARS)
+
+//Tweet Button 
+
+function tweet(){
+  var url = 'https://twitter.com/intent/tweet';
+  var website = 'http://howmuchtimehaveiwasted.com/'
+  var text = "I've wasted " + WASTED +  " percent of my life in front of a screen."
+
+  window.open(url+ "?text="+ text + ";url=" + website, '_blank', 'width=640,height=460','toolbar=no,menubar=no,scrollbars=no,location=no,status=no')
+}
